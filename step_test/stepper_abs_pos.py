ccac291a9804d341b5fd296d879f79b6c7e9c31c
mixdimmens <maximandrediamond@mgail.com>
2019-02-18 03:13:44 -0500

    added method for moving stepper based on absolute coordinate and made the class a child class of A4988Nema in RpiMotorLib

--- step_test/stepper_abs_pos.py
+++ step_test/stepper_abs_pos.py
@@ -1,25 +1,30 @@
 ## hey, this one works! imagine that, sitting down and planning makes things go way smoother ##
 ## code to track absolute angle of stepper shaft based on origin position, or on zereoed coordinate ##
+import RpiMotorLib
 
-class StepperAbsPos:
+
+class StepperAbsPos(A4988Nema):
     def __init__(self, stepper_position):
         self.stepper_position = stepper_position
 
     def new_home(self):
         self.stepper_position = 0
 
+    ## tracks movement of an object in a revolution ##
+    ## will need to modify so it can feed into abs_move_calc
     def abs_position(self, steps, direction):
+        steps_per_rev = 200
         self.steps = steps
         self.direction = direction
-        if self.stepper_position > 200:
-            print('problem! motor position is {}, which is greater than 200'.format(self.stepper_position))
+        if self.stepper_position > steps_per_rev:
+            print('problem! motor position is {}, which is greater than the number of steps per revolution ({})'.format(self.stepper_position, steps_per_rev))
         else:
             if direction == True:
                 self.stepper_position = self.stepper_position + self.steps
-                if self.stepper_position == 200:
+                if self.stepper_position == steps_per_rev:
                     self.stepper_position = 0
-                elif self.stepper_position > 200:
-                    reducer = (self.stepper_position // 200) * 200
+                elif self.stepper_position > steps_per_rev:
+                    reducer = (self.stepper_position // steps_per_rev) * steps_per_rev
                     self.stepper_position = self.stepper_position - reducer
             
             else:
@@ -27,18 +32,43 @@ class StepperAbsPos:
                 print(self.stepper_position)
                 if self.stepper_position < 0:
                     if self.stepper_position >= -199:
-                        self.stepper_position = 200 + self.stepper_position
+                        self.stepper_position = steps_per_rev + self.stepper_position
                     else:
-                        reducer = (self.stepper_position // -200) * 200
+                        reducer = (self.stepper_position // -steps_per_rev) * steps_per_rev
                         print(reducer)
-                        self.stepper_position = 200 + (self.stepper_position + reducer)
-        if self.stepper_position == 200:
+                        self.stepper_position = steps_per_rev + (self.stepper_position + reducer)
+        if self.stepper_position == steps_per_rev:
             self.stepper_position = 0
+
+    ## figures out the difference between where the motor is and where the motor wants to go.
+    ## two modes, shortest path, and user chosen direction of rotation.
+    def abs_move_calc(self, end_position):
+        self.end_position = end_position
+        
+        self.abs_pos_to_home = self.steps - self.stepper_position
+        self.end_position_to_home = self.steps - self.end_position
+
+        if self. abs_pos_to_home == self.end_position_to_home:
+            pass
+        elif self.abs_pos_to_home > self.end_position_to_home:
+            self.direction = True
+            self.abs_move = self.stepper_position - self.end_position
+        elif self.end_position_to_home > self.abs_pos_to_home:
+            self.direction = False
+            self.abs_move = self.end_position - self.stepper_position
+        
+        try:
+            self.motor_go(self.direction, self.steptype, self.abs_move, self.stepdelay, self.verbose, self.initdelay)
+
+            self.abs_position(self.abs_move, self.direction)
+        
+        except:
+            KeyboardInterrupt
 
     def __repr__(self):
         return '{}'.format(self.stepper_position)
 
-stepper = StepperAbsPos(0)
+stepper = StepperAbsPos(201)
 
 stepper.abs_position(635, False)
 print('stepper position 1: {}'.format(stepper))
